164d928c2139635b325ae234b8ea0ec2110ed218
Tinmarino <tinmarino@gmail.com>
2024-06-07 13:30:45 -0400

    Clean task demo

--- script/task-demo.sh
+++ script/task-demo.sh
@@ -3,18 +3,30 @@
 path_task=~/.local/var/task 
 
 task_demo(){
-  parse_argument "$@";
+  : 'Main dispatcher'
+  [[ -d "$path_task" ]] || mkdir -p "$path_task"
+  while (( 0 != $# )); do
+    case $1 in
+      a|add) shift; task_add "$@"; return $?;;
+      r|rm|delete) shift; task_delete "$@"; return $?;;
+      l|list|ls) shift; task_list "$@"; return $?;;
+      *)
+        task_add "$@"; return $?;;
+    esac
+  done
   return $?
 }
 
 
 task_add(){
+  : 'Touch'
   file_task=$(date +'%Y-%m-%dT%H:%M:%S').task
   echo "$*" > "$path_task/$file_task"
 }
 
 
 task_delete(){
+  : 'Rm'
   task=$(get_task_path "$1")
   # Clause: task must exist
   if [[ ! -e "$task" ]]; then
@@ -27,6 +39,7 @@ task_delete(){
 
 
 task_list(){
+  : 'Ls'
   for file in "$path_task"/*.task; do
     ts=${file##"$path_task/"}
     ts=${ts%%.task}
@@ -39,20 +52,6 @@ task_list(){
 
 get_task_path(){ echo -n "$path_task/$1.task"; }
 
-
-parse_argument(){
-  : 'Add dispatch'
-  [[ -d "$path_task" ]] || mkdir -p "$path_task"
-  while (( 0 != $# )); do
-    case $1 in
-      a|add) shift; task_add "$@"; return $?;;
-      r|rm|delete) shift; task_delete "$@"; return $?;;
-      l|list|ls) shift; task_list "$@"; return $?;;
-      *)
-        task_add "$@"; return $?;;
-    esac
-  done
-}
 
 if ! (return 0 2>/dev/null); then
   task_demo "$@"; res=$?
